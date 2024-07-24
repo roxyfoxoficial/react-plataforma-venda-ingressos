22698c13edb23ef3e014d5b24c72dc5ae7d5e5c3
Alan Lima da Silva Guerra <alanlimadasilvaguerra@outlook.com.br>
2024-07-24 20:18:58 -0300

    criando navbar

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,11 +1,52 @@
 // eslint-disable-next-line react/prop-types
 export function Layout({ children }) {
+  const groupUser = "user";
+
   return (
     <>
-      <header></header>
-      <main>
-        {children}
-      </main>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+          <img
+            src="/logo.png"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            alt="Titulo"
+          />
+          Venda de Ingressos Online
+        </a>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <a className="nav-link" href="/">
+                Eventos
+              </a>
+            </li>
+            {groupUser === "admin" &&
+              (
+                <li className="nav-item">
+                  <a className="nav-link" href="/create-event">
+                    Organizar
+                  </a>
+                </li>
+              )}
+            {(groupUser === null && (
+              <li className="nav-item">
+                <a className="nav-link" href="/signin">
+                  Entrar
+                </a>
+              </li>
+            )) || (
+              <li className="nav-item">
+                <a className="nav-link" href="/profile">
+                  Perfil
+                </a>
+              </li>
+            )}
+          </ul>
+        </div>
+      </nav>
+      <main>{children}</main>
     </>
   );
 }
