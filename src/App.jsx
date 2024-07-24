14750d85a183738ed64eff9f9a4b1ca622f9fbca
Alan Lima da Silva Guerra <alanlimadasilvaguerra@outlook.com.br>
2024-07-24 19:49:25 -0300

    criando as páginas do site

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import ChangePassword from "./pages/ChangePassword";
+import CreateEvent from "./pages/CreateEvent";
+import CreateStaff from "./pages/CreateStaff";
+import DetailsEvent from "./pages/DetailsEvent";
+import Profile from "./pages/Profile";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/change-password" element={<ChangePassword />} />
+        <Route path="/create-event" element={<CreateEvent />} />
+        <Route path="/create-staff" element={<CreateStaff />} />
+        <Route path="/details-event/:id" element={<DetailsEvent />} />
+        <Route path="/profile" element={<Profile />} />
         <Route path="**" element={<Home />} />
       </Routes>
     </BrowserRouter>
---
+++ src/pages/CreateStaff.jsx
@@ -0,0 +1,7 @@
+export default function CreateStaff() {
+  return (
+    <div>
+      <h1>Create Staff</h1>
+    </div>
+  );
+}
