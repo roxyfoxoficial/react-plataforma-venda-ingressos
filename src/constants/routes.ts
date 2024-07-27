a8f2583000d16517ff4c3c2c15d154590df877ae
Alan Lima da Silva Guerra <alanlimadasilvaguerra@outlook.com.br>
2024-07-27 19:59:20 -0300

    criando enum com as rotas

---
+++ src/constants/routes.ts
@@ -0,0 +1,13 @@
+export const routes = {
+  home: "/",
+  login: "/signin",
+  register: "/signup",
+  changePassword: "/change-password",
+  createEvent: "/admin/events/create",
+  detailsEvent: "/events/:id",
+  listEvents: "/admin/events",
+  editEvent: "/admin/events/edit/:id",
+  createStaff: "/admin/events/staff/create",
+  listStaffs: "/admin/events/staff",
+  profile: "/profile",
+};
