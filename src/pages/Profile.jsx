3a8b546c0bca41c342f81caf6798cfcb7d82f7c5
gustavo <soaresgustavo07@hotmail.com>
2024-07-29 20:34:16 -0300

    remove @app

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -7,13 +7,19 @@ export default function Profile() {
         <div className="col-md-4 text-center">
           <img src="https://www.hollywoodreporter.com/wp-content/uploads/2022/08/Milly-Alcock-Publicity-H-2022-House-of-the-Dragon-HBOMax-Ollie-Upton-HBO.jpg?w=1296&h=730&crop=1" className="rounded-circle img-fluid" alt="Profile Picture" />
         </div>
-        <div className="col-md-8">
-          <h3 className="mt-4">Rhaenyra Targaryen</h3>
-          <p className="lead">rainha</p>
-          <p>Gioania, CA</p>
-          <p> Rhaenyra Targaryen@example.com</p>
-          <p> (123) 456-7890</p>
-        </div>
+  
+                            <div className="col-lg-6 px-xl-10">
+                                <div className="bg-secondary d-lg-inline-block py-1-9 px-1-9 px-sm-6 mb-1-9 rounded">
+                                    <h3 className="h2 text-white mb-0">Rainha</h3>
+                      
+                                </div>
+                                <ul className="list-unstyled mb-1-9">
+                                    <li className="mb-2 mb-xl-3 display-28"><span className="display-26 text-secondary me-2 font-weight-600">Perfil:</span> Cliente</li>
+                                    <li className="mb-2 mb-xl-3 display-28"><span className="display-26 text-secondary me-2 font-weight-600">cidade:</span> goiania</li>
+                                    <li className="mb-2 mb-xl-3 display-28"><span className="display-26 text-secondary me-2 font-weight-600">Email:</span> edith@mail.com</li>
+                                    <li className="display-28"><span className="display-26 text-secondary me-2 font-weight-600">Telefone:</span> 88847-4848</li>
+                                </ul>
+                            </div>
       </div>
       <div className="row mt-5">
         <div className="col-md-12">
@@ -26,9 +32,9 @@ export default function Profile() {
       </div>
       <div className="row mt-3">
         <div className="col-md-12">
-          <h4>Privacidade</h4>
+          <h4>Eventos</h4>
           <div className="container mt-5">
-          <p> <a href="#" className="btn btn-light">Termos e politicas</a> </p> 
+          <p> <a href="#" className="btn btn-light">Rock in Rio</a> </p> 
           </div>
          </div>
       </div>
